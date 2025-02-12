7bb3c0056e5b254a1b2534600e959f7857fdda53
ankita <ankita>
2025-02-13 00:34:46 +0530

    Added Cost

--- src/productAPI/Models/ProductDetails.cs
+++ src/productAPI/Models/ProductDetails.cs
@@ -4,4 +4,5 @@ public class ProductDetails
     public string? Name { get; set; }
     public string? Type { get; set; }
     public int Inventory { get; set; }
+    public decimal Cost { get; set; }
 }
